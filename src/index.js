8321746c3269c9b8f88336580f64aa56ff44b6f6
luivia <luivia.veiga@gmail.com>
2022-10-19 13:34:44 -0400

    up do projeto Filmes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+  </React.StrictMode>, document.getElementById('root')
 );
